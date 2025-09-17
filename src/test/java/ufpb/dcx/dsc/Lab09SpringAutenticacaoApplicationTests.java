261fe614878bd305283b80ef950eb536266d0c82
cidy <cidywallace12@gmail.com>
2025-09-17 09:43:35 -0300

    first commit

---
+++ src/test/java/ufpb/dcx/dsc/Lab09SpringAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.dcx.dsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab09SpringAutenticacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
